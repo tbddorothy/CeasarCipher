3eebe53695ab301c46f16eda08e62ec4cdebbad6
tbddorothy <dimphomahoko@gmail.com>
2017-10-10 13:14:38 +0200

    Very Basic Encryption

--- Assignment 4/ceasar.js
+++ Assignment 4/ceasar.js
@@ -1,5 +1,21 @@
+
 function encipher(){
-    alert("Encipher");
+    var shiftval = 3;
+    var text1 = document.getElementById('input').value;
+    var textArr = [...text1];
+    var newArr = [...text1];
+    var size = textArr.length;
+    var curr = '';
+
+    for(i = 0; i < size; i++){
+        curr = textArr[i];
+        if(curr != ' '){
+            curr = (curr.charCodeAt(0) + shiftval);
+            newArr[i] = String.fromCharCode(curr);
+        }
+    }
+    document.getElementById("response").innerHTML = newArr.join('');
+
 }
 
 function decipher(){
