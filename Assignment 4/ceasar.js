62d525c8b8fea3fa76c4c7db28817dc12d075a22
tbddorothy <dimphomahoko@gmail.com>
2017-10-10 16:52:53 +0200

    function update

--- Assignment 4/ceasar.js
+++ Assignment 4/ceasar.js
@@ -1,6 +1,6 @@
-
+var shiftval = 0;
 function encipher(){
-    var shiftval = 3;
+    shiftval = parseInt(document.getElementById('shift').value);
     var text1 = document.getElementById('input').value;
     var textArr = [...text1];
     var newArr = [...text1];
@@ -9,8 +9,9 @@ function encipher(){
 
     for(i = 0; i < size; i++){
         curr = textArr[i];
-        if(curr != ' '){
-            curr = (curr.charCodeAt(0) + shiftval);
+        if(curr.charCodeAt(0) > 58 && curr != ' '){
+            curr = curr.charCodeAt(0);
+            curr = convert(curr+shiftval);
             newArr[i] = String.fromCharCode(curr);
         }
     }
@@ -20,4 +21,31 @@ function encipher(){
 
 function decipher(){
     alert("Decipher");
+}
+
+function convert(curr){
+    
+    var temp =  curr - shiftval;
+    if(temp > 64 && temp < 91){
+        curr = ((curr - 64 ) % 26) + 64;
+         if(curr == 64)
+            curr = 90;
+    }
+
+     if(temp > 96 && temp < 123){
+        curr = ((curr - 96 ) % 26) + 96;
+        if(curr == 96)
+            curr = 122;
+    }
+
+  /*  if(temp > 47 && temp < 58){
+        curr = ((curr - 47) % 10) + 47;
+        if(curr == 47)
+            curr = 57;
+    }*/
+    
+    
+
+    return curr;
+    
 }
