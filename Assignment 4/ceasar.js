033496fecf30f8e3a9eb9d452ffeb849f24d9f0e
tbddorothy <dimphomahoko@gmail.com>
2017-10-10 19:00:37 +0200

    final touches

--- Assignment 4/ceasar.js
+++ Assignment 4/ceasar.js
@@ -21,7 +21,22 @@ function encipher(){
 }
 
 function decipher(){
-    alert("Decipher");
+    shiftval = parseInt(document.getElementById('shift').value);
+    set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    set = [...set];
+    var text1 = document.getElementById('input').value;
+    var textArr = [...text1];
+    var newArr = [...text1];
+    var size = textArr.length;
+    var curr = '';
+
+    for(i = 0; i < size; i++){
+            curr = textArr[i];
+            curr = DfindIndex(curr);
+            curr = (curr) % 62;
+            newArr[i] = set[curr];
+    }
+    document.getElementById("response").innerHTML = newArr.join('');
 }
 
 
@@ -37,4 +52,28 @@ function findIndex(curr){
     }
   
     return i + shiftval - 1;
+}
+
+function DfindIndex(curr){
+    
+    var found = false;
+    var i = 0;
+    var ret;
+    while(!found && i < 63){
+        if(curr == set[i]){
+            found = true;
+            ret = i - shiftval;
+            if(ret < 0){
+                return 62 + ret;
+            }
+
+            return ret;
+        }
+        i = i + 1;
+
+       
+    }
+  
+     return;
+    
 }
