468002d672409ddefe7fcec7248eef4f6c9f572c
tbddorothy <dimphomahoko@gmail.com>
2017-10-10 17:53:57 +0200

    fully functional ceasar

--- Assignment 4/ceasar.js
+++ Assignment 4/ceasar.js
@@ -1,6 +1,9 @@
+var set;
 var shiftval = 0;
 function encipher(){
     shiftval = parseInt(document.getElementById('shift').value);
+    set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+    set = [...set];
     var text1 = document.getElementById('input').value;
     var textArr = [...text1];
     var newArr = [...text1];
@@ -8,12 +11,10 @@ function encipher(){
     var curr = '';
 
     for(i = 0; i < size; i++){
-        curr = textArr[i];
-        if(curr.charCodeAt(0) > 58 && curr != ' '){
-            curr = curr.charCodeAt(0);
-            curr = convert(curr+shiftval);
-            newArr[i] = String.fromCharCode(curr);
-        }
+            curr = textArr[i];
+            curr = findIndex(curr);
+            curr = (curr) % 62;
+            newArr[i] = set[curr];
     }
     document.getElementById("response").innerHTML = newArr.join('');
 
@@ -23,29 +24,17 @@ function decipher(){
     alert("Decipher");
 }
 
-function convert(curr){
+
+function findIndex(curr){
     
-    var temp =  curr - shiftval;
-    if(temp > 64 && temp < 91){
-        curr = ((curr - 64 ) % 26) + 64;
-         if(curr == 64)
-            curr = 90;
+    var found = false;
+    var i = 0;
+    while(!found && i < 63){
+        if(curr == set[i]){
+            found = true;
+        }
+        i = i + 1;
     }
-
-     if(temp > 96 && temp < 123){
-        curr = ((curr - 96 ) % 26) + 96;
-        if(curr == 96)
-            curr = 122;
-    }
-
-  /*  if(temp > 47 && temp < 58){
-        curr = ((curr - 47) % 10) + 47;
-        if(curr == 47)
-            curr = 57;
-    }*/
-    
-    
-
-    return curr;
-    
+  
+    return i + shiftval - 1;
 }
